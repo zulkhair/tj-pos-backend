1e8bb854b3c546fcd807ebe268edee405fb16338
Zulkhair Abdullah Daim <zulkhair.daim@gmail.com>
2022-03-09 00:15:45 +0700

    tidy up ability in seed.sql

--- schema/seed.sql
+++ schema/seed.sql
@@ -15,7 +15,9 @@ VALUES ('web:user:createUser', 'web:user', 'Tambah Data', 0, '/user/register-use
        ('web:user:editUser', 'web:user', 'Ubah Data', 1, '/user/edit-user.html', 'fas fa-pen'),
        ('web:role:createRole', 'web:role', 'Tambah Data', 0, '/role/create-role.html', 'fas fa-plus'),
        ('web:role:editRole', 'web:role', 'Ubah Data', 1, '/role/edit-role.html', 'fas fa-pen'),
-       ('web:masterdata:product', 'web:masterdata', 'Produk', 0, '/master/product.html', 'fas fa-seedling')
+       ('web:masterdata:product', 'web:masterdata', 'Produk', 0, '/master/product.html', 'fas fa-seedling'),
+       ('web:masterdata:supplier', 'web:masterdata', 'Supplier', 1, '/master/supplier.html', 'fas fa-people-carry-box'),
+       ('web:masterdata:customer', 'web:masterdata', 'Customer', 2, '/master/customer.html', 'fas fa-user-tag')
 ;
 
 ------------ PERMISSION -------------
@@ -27,7 +29,13 @@ VALUES ('web:user:createUser', 'web:user:createUser', 'Registrasi', 0, '/api/rol
        ('web:role:editRole', 'web:role:editRole', 'Ubah Data', 1, '/api/role/find-all;/api/role/edit'),
        ('web:masterdata:product:add', 'web:masterdata:product', 'Tambah Data Produk', 0, '/api/product/find;/api/product/create;/api/auth/check'),
        ('web:masterdata:product:view', 'web:masterdata:product', 'Lihat Data Produk', 1, '/api/product/find;/api/auth/check'),
-       ('web:masterdata:product:edit', 'web:masterdata:product', 'Perbarui Data Produk', 2, '/api/product/find;/api/product/edit;/api/auth/check')
+       ('web:masterdata:product:edit', 'web:masterdata:product', 'Perbarui Data Produk', 2, '/api/product/find;/api/product/edit;/api/auth/check'),
+       ('web:masterdata:supplier:add', 'web:masterdata:supplier', 'Tambah Data Supplier', 0, '/api/supplier/find;/api/supplier/create;/api/auth/check'),
+       ('web:masterdata:supplier:view', 'web:masterdata:supplier', 'Lihat Data Supplier', 1, '/api/supplier/find;/api/auth/check'),
+       ('web:masterdata:supplier:edit', 'web:masterdata:supplier', 'Perbarui Data Supplier', 2, '/api/supplier/find;/api/supplier/edit;/api/auth/check'),
+       ('web:masterdata:customer:add', 'web:masterdata:customer', 'Tambah Data Customer', 0, '/api/customer/find;/api/customer/create;/api/auth/check'),
+       ('web:masterdata:customer:view', 'web:masterdata:customer', 'Lihat Data Customer', 1, '/api/customer/find;/api/auth/check'),
+       ('web:masterdata:customer:edit', 'web:masterdata:customer', 'Perbarui Data Customer', 2, '/api/customer/find;/api/customer/edit;/api/auth/check')
 ;
 
 ----------------- ROLE ---------------
@@ -42,7 +50,15 @@ INSERT INTO role_permission(role_id, permission_id)
 VALUES ('735c7b8b96a8463c8493037d4c8ff085', 'web:user:createUser'),
        ('735c7b8b96a8463c8493037d4c8ff085', 'web:user:editUser'),
        ('735c7b8b96a8463c8493037d4c8ff085', 'web:role:createRole'),
-       ('735c7b8b96a8463c8493037d4c8ff085', 'web:role:editRole')
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:product:add'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:product:view'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:product:edit'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:supplier:add'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:supplier:view'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:supplier:edit'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:customer:add'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:customer:view'),
+       ('735c7b8b96a8463c8493037d4c8ff085', 'web:masterdata:customer:edit')
 ;
 
 ----------------- USER ---------------
