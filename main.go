afbc731ee201f9cc1b7313aa7529f1b229abd6a9
Zulkhair Abdullah Daim <zulkhair.daim@gmail.com>
2023-04-07 14:01:26 +0700

    ganti sorting kontrabon print

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	fmt.Println("POS Backend v1.0.0")
+	fmt.Println("POS Backend v1.0.2")
 	// load configuration
 	fmt.Println("Load Configuration")
 	cfg, err := config.New("config.yaml")
