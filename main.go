e1b9be0e979a85c1bbd7b3b55b256b778f35efcb
zulkhair <zulkhair.daim@gmail.com>
2023-10-22 17:01:39 +0700

    chore : update version to 1.0.5

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	fmt.Println("POS Backend v1.0.4")
+	fmt.Println("POS Backend v1.0.5")
 	// load configuration
 	fmt.Println("Load Configuration")
 	cfg, err := config.New("config.yaml")
